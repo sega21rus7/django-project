763759d9a4d51779a328dbc4101204074f0c2f4b
nikonorov <gts5660gio@gmail.com>
2019-07-01 17:17:13 +0300

    Упрощен шаблон + форматирование

--- frontend/src/apps/chat/AppSignIn.vue
+++ frontend/src/apps/chat/AppSignIn.vue
@@ -1,44 +1,40 @@
 <template>
-  <div>
-
-    <v-container fill-height fluid>
-      <v-layout align-center justify-center>
-        <v-flex md4 sm8 xs12>
-          <v-card class="elevation-12">
-            <v-toolbar color="primary" dark>
-              <v-toolbar-title>Sign in form</v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
-              <v-form v-model="valid">
-                <v-text-field
-                  :rules="[rules.required, rules.username]" clearable label="Login"
-                  name="login" prepend-icon="person" required type="text" v-model="username">
-                </v-text-field>
-                <v-text-field :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-                              :rules="[rules.required].concat(rules.password)"
-                              :type="showPassword ? 'text' : 'password'"
-                              @click:append="showPassword = !showPassword" clearable id="password" label="Password"
-                              name="password"
-                              prepend-icon="lock" required v-model="password">
-                </v-text-field>
-                <v-alert :value="true"
-                         type="error"
-                         v-if="errors"
-                >
-                  {{errors}}
-                </v-alert>
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn @click="signIn" color="primary">Sign in</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-
-  </div>
+  <v-container fill-height fluid>
+    <v-layout align-center justify-center>
+      <v-flex md4 sm8 xs12>
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark>
+            <v-toolbar-title>Sign in form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid">
+              <v-text-field
+                :rules="[rules.required, rules.username]" clearable label="Login"
+                name="login" prepend-icon="person" required type="text" v-model="username">
+              </v-text-field>
+              <v-text-field :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                            :rules="[rules.required].concat(rules.password)"
+                            :type="showPassword ? 'text' : 'password'"
+                            @click:append="showPassword = !showPassword" clearable id="password" label="Password"
+                            name="password"
+                            prepend-icon="lock" required v-model="password">
+              </v-text-field>
+              <v-alert :value="true"
+                       type="error"
+                       v-if="errors"
+              >
+                {{errors}}
+              </v-alert>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="signIn" color="primary">Sign in</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -56,7 +52,9 @@
         showConfirmPassword: false,
         rules: {
           required: value => !!value || 'This field must be required.',
-          username: v => {return v.length >= 5 || 'Login must be more than 8 characters or equal to 5'},
+          username: v => {
+            return v.length >= 5 || 'Login must be more than 8 characters or equal to 5'
+          },
           password: [
             v => v.length >= 6 || 'Password must be more than 8 characters or equal to 6',
             v => v.length <= 32 || 'Password must be less than 32 characters or equal to 32',
