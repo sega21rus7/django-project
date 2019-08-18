8d1460235160219fd9b289a8b6d7204c2b7af5ba
sergeevav <sega21rus98@mail.ru>
2019-08-18 20:56:14 +0300

    Доработаны тесты для профиля user

--- backend/user_profile/tests/test_views.py
+++ backend/user_profile/tests/test_views.py
@@ -17,13 +17,6 @@ class UserCreate(APITestCase):
         self._create_user()
 
 
-class IndexViewTest(APITestCase):
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('chat:index'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'chat/index.html')
-
-
 # исправить ошибку в самом функционале, стр. возвращает код 302
 class UserSignOutViewTest(UserCreate):
     def setUp(self):
@@ -41,7 +34,7 @@ class UserViewTest(UserCreate):
         super().setUp()
 
     def test_get_users_list(self):
-        response = self.client.get(reverse('chat:select_user'))
+        response = self.client.get(reverse('user_profile:select_user'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 1)
         self.assertEqual(response.data['next'], None)
@@ -58,7 +51,7 @@ class UserCreateViewTest(APITestCase):
         }
 
     def test_create_user(self):
-        resp = self.client.post(reverse('chat:create_user'), data=self.data)
+        resp = self.client.post(reverse('user_profile:create_user'), data=self.data)
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
         # доделать, исправить ошибку
         self.assertEqual(resp.data, self.data)
@@ -73,7 +66,7 @@ class UserLoginViewTest(UserCreate):
         }
 
     def test_user_login(self):
-        resp = self.client.post(reverse('chat:login_user'), data=self.data)
+        resp = self.client.post(reverse('user_profile:login_user'), data=self.data)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertTrue('user' in resp.data.values())
 
@@ -90,62 +83,12 @@ class UserUpdateDeleteViewTest(UserCreate):
             'password': 'new_password',
             'confirm_password': 'new_password',
         }
-        response = self.client.put(reverse('chat:update_user', kwargs={'pk': self.user.pk}),
+        response = self.client.put(reverse('user_profile:update_user', kwargs={'pk': self.user.pk}),
                                    data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, data)
 
     def test_delete_user(self):
-        response = self.client.delete(reverse('chat:update_user', kwargs={'pk': self.user.pk}))
+        response = self.client.delete(reverse('user_profile:update_user', kwargs={'pk': self.user.pk}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-
-class ChatMessageTest(UserCreate):
-    def setUp(self):
-        super().setUp()
-        self.message = ChatMessage.objects.create(sender=self.user, message='hello', pub_date=datetime.now())
-
-    def test_get_message_list(self):
-        response = self.client.get(reverse('chat:select_message'))
-        serializer = ChatMessageSerializer(instance=self.message)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['count'], 1)
-        self.assertEqual(response.data['next'], None)
-        self.assertEqual(response.data['previous'], None)
-        # нужно доработать
-        self.assertEqual(response.data['results'], serializer.data)
-
-
-class ChatMessageCreateTest(UserCreate):
-    def test_create_message(self):
-        super().setUp()
-        self.client.login(username='user', password='password')
-        response = self.client.post(reverse('chat:create_message'), data={'message': 'message'})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # доработать
-        self.assertEqual(response.data, 'message')
-
-
-class ChatMessageUpdateTest(UserCreate):
-    def setUp(self):
-        super().setUp()
-        self.client.login(username='user', password='password')
-        self.message = ChatMessage.objects.create(sender=self.user, message='hello', pub_date=datetime.now())
-
-    def test_update_message(self):
-        data = {'message': 'message'}
-        response = self.client.put(reverse('chat:update_message', kwargs={'pk': self.message.pk}),
-                                   data=data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, data)
-
-
-class ChatMessageDeleteTest(UserCreate):
-    def setUp(self):
-        super().setUp()
-        self.client.login(username='user', password='password')
-        self.message = ChatMessage.objects.create(sender=self.user, message='hello', pub_date=datetime.now())
-
-    def test_delete_message(self):
-        response = self.client.delete(reverse('chat:update_message', kwargs={'pk': self.message.pk}))
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
