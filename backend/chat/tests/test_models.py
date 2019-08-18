9bf1d4edbfe2345457f1cd0252a677a64e088ff8
sergeevav <sega21rus98@mail.ru>
2019-08-18 18:05:49 +0300

    оптимизированы юнит-тесты на модели

--- backend/chat/tests/test_models.py
+++ backend/chat/tests/test_models.py
@@ -8,14 +8,9 @@ from chat.models import ChatMessage
 
 class ChatMessageTest(TestCase):
     def setUp(self):
-        man = User.objects.create_user(username='man', email='man@mail.ru', password='')
-        women = User.objects.create_user(username='woman', email='women@mail.ru', password='')
-
-        ChatMessage.objects.create(sender=man, message='hi guys', pub_date=datetime.now())
-        ChatMessage.objects.create(sender=women, message='hello', pub_date=datetime.now())
+        user = User.objects.create_user(username='user', email='user@mail.ru', password='')
+        ChatMessage.objects.create(sender=user, message='hi guys', pub_date=datetime.now())
 
     def test_message(self):
-        man_message = ChatMessage.objects.get(message='hi guys')
-        women_message = ChatMessage.objects.get(message='hello')
-        self.assertEqual(man_message.sender.username, 'man')
-        self.assertEqual(women_message.sender.username, 'woman')
+        message = ChatMessage.objects.get(message='hi guys')
+        self.assertEqual(message.sender.username, 'man')
